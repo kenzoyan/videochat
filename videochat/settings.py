c5db50245c1075e7e73c0e087ee73eb6ad34cb9f
Kenzo <570704104@qq.com>
2022-02-09 18:31:14 +0200

    first commit

--- videochat/settings.py
+++ videochat/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,9 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+
 
 django_heroku.settings(locals())
 
